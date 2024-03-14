2fb27f1d686f409eff4561698e96bfb0056f3362
老鸿业 <1004591163@qq.com>
2024-03-14 10:57:34 +0800

    feat:新增rabbitmq死信队列

--- src/main/java/com/example/demo/controller/RabbitMQController.java
+++ src/main/java/com/example/demo/controller/RabbitMQController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -25,10 +27,19 @@ public class RabbitMQController {
 
     private final static String QUEUE_NAME = "test";
 
+    //扇出交换机
     private final static String FANOUT_EXCHANGE_NAME = "fanout-exchange";
     private final static String FANOUT_QUEUE1_NAME = "fanout-queue1";
     private final static String FANOUT_QUEUE2_NAME = "fanout-queue2";
     private final static String FANOUT_ROUTING_KEY = "fanout-routing-key";
+
+    //死信队列
+    private final static String NORMAL_QUEUE_NAME = "normal-queue";
+    private final static String NORMAL_EXCHANGE_NAME = "normal-exchange";
+    private final static String DEAD_QUEUE_NAME = "dead-queue";
+    private final static String DEAD_EXCHANGE_NAME = "dead-exchange";
+    private final static String NORMAL_EXCHANGE_ROUTING_KEY = "normal-exchange-routing-key";
+    private final static String DEAD_EXCHANGE_ROUTING_KEY = "dead-exchange-routing-key";
 
     @GetMapping("/test")
     public void test() throws Exception {
@@ -41,15 +52,11 @@ public class RabbitMQController {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        try {
-            channel.exchangeDeclare("test-exchange", BuiltinExchangeType.FANOUT);
-            channel.queueDeclare("test-queue1", true, false, false, null);
-            channel.queueDeclare("test-queue2", true, false, false, null);
-            channel.queueBind("test-queue1", "test-exchange", "");
-            channel.queueBind("test-queue2", "test-exchange", "");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        channel.exchangeDeclare("test-exchange", BuiltinExchangeType.FANOUT);
+        channel.queueDeclare("test-queue1", true, false, false, null);
+        channel.queueDeclare("test-queue2", true, false, false, null);
+        channel.queueBind("test-queue1", "test-exchange", "");
+        channel.queueBind("test-queue2", "test-exchange", "");
     }
 
     @GetMapping("/simpleModeProduce")
@@ -64,52 +71,48 @@ public class RabbitMQController {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        try {
-            /**
-             * 声明一个队列
-             * 1. 队列名称
-             * 2. 队列里的消息是否持久化 默认存储在内存中
-             * 3. 队列是否只供一个消费者进行消费 是否共享给多个消费者消费
-             * 4. 是否在最后一个消费者断开连接后队列自动删除
-             * 5. 其他参数
-             */
-            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        /**
+         * 声明一个队列
+         * 1. 队列名称
+         * 2. 队列里的消息是否持久化 默认存储在内存中
+         * 3. 队列是否只供一个消费者进行消费 是否共享给多个消费者消费
+         * 4. 是否在最后一个消费者断开连接后队列自动删除
+         * 5. 其他参数
+         */
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
 
-            channel.confirmSelect();
-            //监听消息确认成功的回调
-            ConfirmCallback ackCallback = (deliveryTag, ackSignal) -> {
-                log.info("消息发布确认成功");
-                if(ackSignal) {
-                    log.info("发布确认成功");
-                }
-            };
-            //监听消息确认失败的回调
-            ConfirmCallback nackCallback = (deliveryTag, ackSignal) -> {
-                log.info("消息发布确认失败");
-                if(ackSignal) {
-                    log.info("发布确认失败");
-                }
-            };
-            channel.addConfirmListener(ackCallback, nackCallback);
-
-            /**
-             * 发送一个消息到队列
-             * 1. 发送到哪个交换机
-             * 2. 路由key，路由到哪个队列
-             * 3. 其他参数
-             * 4. 发送消息的消息体
-             */
-            channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, getRandomString(10).getBytes());
-            /**
-            boolean publishResult = channel.waitForConfirms();
-            if(publishResult) {
-                log.info("消息持久化完毕");
+        channel.confirmSelect();
+        //监听消息确认成功的回调
+        ConfirmCallback ackCallback = (deliveryTag, ackSignal) -> {
+            log.info("消息发布确认成功");
+            if(ackSignal) {
+                log.info("发布确认成功");
             }
-             **/
-            log.info("消息发送完毕");
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
+        };
+        //监听消息确认失败的回调
+        ConfirmCallback nackCallback = (deliveryTag, ackSignal) -> {
+            log.info("消息发布确认失败");
+            if(ackSignal) {
+                log.info("发布确认失败");
+            }
+        };
+        channel.addConfirmListener(ackCallback, nackCallback);
+
+        /**
+         * 发送一个消息到队列
+         * 1. 发送到哪个交换机
+         * 2. 路由key，路由到哪个队列
+         * 3. 其他参数
+         * 4. 发送消息的消息体
+         */
+        channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, getRandomString(10).getBytes());
+        /**
+         boolean publishResult = channel.waitForConfirms();
+         if(publishResult) {
+         log.info("消息持久化完毕");
+         }
+         **/
+        log.info("消息发送完毕");
     }
 
 
@@ -126,32 +129,28 @@ public class RabbitMQController {
         Channel channel = connection.createChannel();
 //        channel.basicQos(5);
 
-        try {
-            DeliverCallback deliverCallback = (consumerTag, message) -> {
-                log.info(consumerTag);
-                log.info(new String(message.getBody()));
-                log.info("消费成功");
-                channel.basicAck(message.getEnvelope().getDeliveryTag(), false);
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            log.info(consumerTag);
+            log.info(new String(message.getBody()));
+            log.info("消费成功");
+            channel.basicAck(message.getEnvelope().getDeliveryTag(), false);
 
-                log.info(message.getProperties().toString());
+            log.info(message.getProperties().toString());
 //                channel.basicNack(message.getEnvelope().getDeliveryTag(), false, true);
-            };
+        };
 
-            CancelCallback cancelCallback = consumerTag -> {
-                log.info(consumerTag);
-                log.info("消费被中断");
-            };
+        CancelCallback cancelCallback = consumerTag -> {
+            log.info(consumerTag);
+            log.info("消费被中断");
+        };
 
-            /**
-             * 1. 消费哪个队列
-             * 2. 消费成功后是否需要自动应答
-             * 3. 消息发送过来的回调
-             * 4. 被取消消费的回调
-             */
-            channel.basicConsume(QUEUE_NAME,false, deliverCallback, cancelCallback);
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
+        /**
+         * 1. 消费哪个队列
+         * 2. 消费成功后是否需要自动应答
+         * 3. 消息发送过来的回调
+         * 4. 被取消消费的回调
+         */
+        channel.basicConsume(QUEUE_NAME,false, deliverCallback, cancelCallback);
     }
 
     //length用户要求产生字符串的长度
@@ -220,5 +219,88 @@ public class RabbitMQController {
     @GetMapping("/directConsume")
     public void directConsume() {
 
+    }
+
+    @GetMapping("/deadLetterConfig")
+    public void deadLetterConfig() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        Map<String, Object> arguments = new HashMap<>();
+        //消息过期TTL，设置整个消息的过期时间
+        arguments.put("x-message-ttl", 1000 * 60 * 30);
+        //设置队列最大容量长度，数量超出的消息则进入死信队列
+        arguments.put("x-max-length", 10);
+        //正常队列设置死信交换机
+        arguments.put("x-dead-letter-exchange", DEAD_EXCHANGE_NAME);
+        //设置死信routingKey
+        arguments.put("x-dead-letter-routing-key", DEAD_EXCHANGE_ROUTING_KEY);
+
+        //声明普通交换机与死信交换机
+        channel.exchangeDeclare(NORMAL_EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        channel.exchangeDeclare(DEAD_EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        //声明普通队列与死信队列
+        channel.queueDeclare(NORMAL_QUEUE_NAME, false, false, false, arguments);
+        channel.queueDeclare(DEAD_QUEUE_NAME, false, false, false, null);
+        //绑定交换机与队列
+        channel.queueBind(NORMAL_QUEUE_NAME, NORMAL_EXCHANGE_NAME, NORMAL_EXCHANGE_ROUTING_KEY);
+        channel.queueBind(DEAD_QUEUE_NAME, DEAD_EXCHANGE_NAME, DEAD_EXCHANGE_ROUTING_KEY);
+
+        channel.close();
+        connection.close();
+    }
+
+    @GetMapping("/deadLetterProduce")
+    public void deadLetterProduce() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.basicPublish(NORMAL_EXCHANGE_NAME, NORMAL_EXCHANGE_ROUTING_KEY, null, getRandomString(10).getBytes());
+
+        channel.close();
+        connection.close();
+    }
+
+    @GetMapping("/deadLetterConsume")
+    public void deadLetterConsume() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.basicQos(10);
+
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            log.info(consumerTag);
+            log.info(new String(message.getBody()));
+            log.info("消费成功");
+            channel.basicAck(message.getEnvelope().getDeliveryTag(), false);
+//            channel.basicNack();
+//            channel.basicReject(message.getEnvelope().getDeliveryTag(), false);
+//            log.info("消费拒绝");
+        };
+
+        CancelCallback cancelCallback = consumerTag -> {
+            log.info(consumerTag);
+            log.info("消费被中断");
+        };
+
+        channel.basicConsume(NORMAL_QUEUE_NAME, false, deliverCallback, cancelCallback);
     }
 }
