726da4ff85327bb3121d890f62469407a32fc565
whats_up222 <1004591163@qq.com>
2024-12-08 21:52:47 +0800

    feat:新增playwright

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.nio.file.Paths;
 
 //dynamic-datasource生效需要如此设置
 //@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
@@ -18,6 +24,13 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+//        try (Playwright playwright = Playwright.create()) {
+//            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+//            Page page = browser.newPage();
+//            page.navigate("https://playwright.dev/");
+//            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+//        }
     }
 
 }
